ebb2ae69a74a9c9af46cb218ca29a51baa25c757
Abdeljalil37 <widadpop@gmail.com>
2023-10-20 23:09:20 +0100

    header file

---
+++ sort.h
@@ -0,0 +1,13 @@
+#ifndef SORT_H
+#define SORT_H
+
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Sorting Algorithms Functions */
+
+void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
