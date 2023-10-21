b9afb631614ced566af11293a83a68e973e03e6a
Abdeljalil37 <widadpop@gmail.com>
2023-10-21 22:48:08 +0100

    shell sort function

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -18,3 +18,29 @@ void _swap(int *array, int i, int j)
 		array[j] = tmp;
 	}
 }
+
+/**
+ * shell_sort - sort the list and print the changes
+ * @array: The array to sort.
+ * @size: Size of the array
+ * Return: Nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t h = 0,  i, j;
+
+	if (size < 2)
+		return;
+
+	while (h <= size / 3)
+		h = h * 3 + 1;
+
+	while (h >= 1)
+	{
+		for (i = h; i < size; i++)
+			for (j = i; j >= h && array[j] < array[j - h]; j -= h)
+				_swap(array, j, j - h);
+		h /= 3;
+		print_array(array, size);
+	}
+}
